a50163b6b398cfe12fb82db2b71b2038a5be687c
Jeff Tian <Jeff.Tian@outlook.com>
2015-01-12 21:03:31 +0800

    html layout.

---
+++ Css/icomoon/ie7/ie7.js
@@ -0,0 +1,39 @@
+/* To avoid CSS expressions while still supporting IE 7 and IE 6, use this script */
+/* The script tag referencing this file must be placed before the ending body tag. */
+
+/* Use conditional comments in order to target IE 7 and older:
+	<!--[if lt IE 8]><!-->
+	<script src="ie7/ie7.js"></script>
+	<!--<![endif]-->
+*/
+
+(function() {
+	function addIcon(el, entity) {
+		var html = el.innerHTML;
+		el.innerHTML = '<span style="font-family: \'icomoon\'">' + entity + '</span>' + html;
+	}
+	var icons = {
+		'icon-shopping-basket': '&#xe600;',
+		'icon-suitcase': '&#xe601;',
+		'icon-envelope': '&#xe605;',
+		'icon-ticket': '&#xe606;',
+		'icon-office': '&#xe602;',
+		'icon-mobile': '&#xe607;',
+		'icon-arrow-down': '&#xe603;',
+		'icon-phone': '&#xe604;',
+		'0': 0
+		},
+		els = document.getElementsByTagName('*'),
+		i, c, el;
+	for (i = 0; ; i += 1) {
+		el = els[i];
+		if(!el) {
+			break;
+		}
+		c = el.className;
+		c = c.match(/icon-[^\s'"]+/);
+		if (c && icons[c[0]]) {
+			addIcon(el, icons[c[0]]);
+		}
+	}
+}());
